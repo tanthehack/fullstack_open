63ab2396711228b17ce5ab9da63ef43d60358623
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-04-17 15:43:16 +0100

    exercise 1.3 step 3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -24,18 +24,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total totalnum={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} />
+      <Total totalnum={part1.exercises + part2.exercises + part2.exercises} />
     </>
   )
 }
